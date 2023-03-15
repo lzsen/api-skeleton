50c8ad46477851d1dc2f65826c63e9539213636e
lzsen <lzs.vip@qq.com>
2023-03-15 12:36:37 +0800

    增加redis 连接有效性判断

--- app/Process/AsyncQueueConsumer.php
+++ app/Process/AsyncQueueConsumer.php
@@ -9,12 +9,30 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace App\Process;
 
+use App\Components\Log;
 use Hyperf\AsyncQueue\Process\ConsumerProcess;
 use Hyperf\Process\Annotation\Process;
+use Hyperf\Redis\Exception\InvalidRedisProxyException;
+use Hyperf\Redis\RedisFactory;
 
 #[Process(name: 'AsyncQueueConsumer')]
 class AsyncQueueConsumer extends ConsumerProcess
 {
+    public function handle(): void
+    {
+        try {
+            $redis = $this->container->get(RedisFactory::class)->get($this->config['redis']['pool'] ?? 'default');
+            $redis->ping();
+            parent::handle(); // $this->driver->consume();
+        } catch (InvalidRedisProxyException $e) {
+            Log::queue()->error(sprintf('[AsyncQueueConsumer]:%s', 'redis 配置不正确'));
+        } catch (\Throwable $e) {
+            Log::queue()->error(sprintf('[AsyncQueueConsumer]:%s', $e->getMessage()));
+        }
+
+    }
+
 }
